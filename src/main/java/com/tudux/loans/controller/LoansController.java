8af0372fc29595facbe710dd39a505b130baa7aa
Alexander Alvarenga <moltron_nano@hotmail.com>
2022-05-11 21:50:22 -0600

    feat: initial microservice setup

---
+++ src/main/java/com/tudux/loans/controller/LoansController.java
@@ -0,0 +1,36 @@
+package com.tudux.loans.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tudux.loans.model.Customer;
+import com.tudux.loans.model.Loans;
+import com.tudux.loans.repository.LoansRepository;
+
+/**
+ * @author Eazy Bytes
+ *
+ */
+
+@RestController
+public class LoansController {
+
+	@Autowired
+	private LoansRepository loansRepository;
+
+	@PostMapping("/myLoans")
+	public List<Loans> getLoansDetails(@RequestBody Customer customer) {
+		List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+		if (loans != null) {
+			return loans;
+		} else {
+			return null;
+		}
+
+	}
+
+}
