397e6e5033669805662fc5a2b9fb735550513a13
Alexander Alvarenga <moltron_nano@hotmail.com>
2022-05-22 15:05:09 -0600

    feat: add config server testing

--- src/main/java/com/tudux/loans/controller/LoansController.java
+++ src/main/java/com/tudux/loans/controller/LoansController.java
@@ -3,12 +3,18 @@ package com.tudux.loans.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.tudux.loans.config.LoansServiceConfig;
 import com.tudux.loans.model.Customer;
 import com.tudux.loans.model.Loans;
+import com.tudux.loans.model.Properties;
 import com.tudux.loans.repository.LoansRepository;
 
 /**
@@ -21,6 +27,9 @@ public class LoansController {
 
 	@Autowired
 	private LoansRepository loansRepository;
+	
+	@Autowired
+	LoansServiceConfig loansConfig;
 
 	@PostMapping("/myLoans")
 	public List<Loans> getLoansDetails(@RequestBody Customer customer) {
@@ -31,6 +40,15 @@ public class LoansController {
 			return null;
 		}
 
+	}
+	
+	@GetMapping("/loans/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(loansConfig.getMsg(), loansConfig.getBuildVersion(),
+				loansConfig.getMailDetails(), loansConfig.getActiveBranches());
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
 	}
 
 }
