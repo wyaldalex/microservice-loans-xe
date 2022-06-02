54075b63d66d32e81a7d555e312f272c045f4427
Alexander Alvarenga <moltron_nano@hotmail.com>
2022-06-01 21:55:07 -0600

    feat: add micrometer support

--- src/main/java/com/tudux/loans/LoansApplication.java
+++ src/main/java/com/tudux/loans/LoansApplication.java
@@ -3,6 +3,10 @@ package com.tudux.loans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 @RefreshScope
@@ -10,6 +14,11 @@ public class LoansApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoansApplication.class, args);
+	}
+	
+	@Bean
+	public TimedAspect timedAspect(MeterRegistry registry) {
+	    return new TimedAspect(registry);
 	}
 
 }
